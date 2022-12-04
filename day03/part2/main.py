acb1337d629f3cfc413a60be61845ace3efdc548
Oren Fromberg <orenfromberg@gmail.com>
2022-12-03 20:46:27 -0500

    spelling

--- day03/part2/main.py
+++ day03/part2/main.py
@@ -2,7 +2,7 @@ total = 0
 with open('./day03/part2/data.txt') as f:
     lines = f.readlines()
     for n in range(0,len(lines),3):
-        intesection = [x for x in lines[n] if x in lines[n+1]]
-        badge = [x for x in intesection if x in lines[n+2]][0]
+        both = [x for x in lines[n] if x in lines[n+1]]
+        badge = [x for x in both if x in lines[n+2]][0]
         total += ord(badge) - ord('a') + 1 if badge.islower() else ord(badge) - ord('A') + 27
     print(total)
