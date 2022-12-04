8443cfeb21f31d42640172305d1d5006d4aef4dd
Oren Fromberg <orenfromberg@gmail.com>
2022-12-03 20:48:29 -0500

    tighter

--- day03/part2/main.py
+++ day03/part2/main.py
@@ -2,7 +2,6 @@ total = 0
 with open('./day03/part2/data.txt') as f:
     lines = f.readlines()
     for n in range(0,len(lines),3):
-        both = [x for x in lines[n] if x in lines[n+1]]
-        badge = [x for x in both if x in lines[n+2]][0]
+        badge = [x for x in [x for x in lines[n] if x in lines[n+1]] if x in lines[n+2]][0]
         total += ord(badge) - ord('a') + 1 if badge.islower() else ord(badge) - ord('A') + 27
     print(total)
