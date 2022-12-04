41d2fc6b03e18b01ef78857eabf46ca1690d6ac7
Oren Fromberg <orenfromberg@gmail.com>
2022-12-03 20:43:01 -0500

    better

--- day03/part2/main.py
+++ day03/part2/main.py
@@ -2,9 +2,7 @@ total = 0
 with open('./day03/part2/data.txt') as f:
     lines = f.readlines()
     for n in range(0,len(lines),3):
-        first = [x for x in lines[n] if x in lines[n+1]]
-        second = [x for x in lines[n+1] if x in lines[n+2]]
-        both = [x for x in first if x in second][0]
-        val = ord(both) - ord('a') + 1 if both.islower() else ord(both) - ord('A')  + 27
-        total += val
+        intesection = [x for x in lines[n] if x in lines[n+1]]
+        badge = [x for x in intesection if x in lines[n+2]][0]
+        total += ord(badge) - ord('a') + 1 if badge.islower() else ord(badge) - ord('A') + 27
     print(total)
