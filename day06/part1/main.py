ca40ac0c4faef32e6c28781f9fded7d70ceb8a96
Oren Fromberg <orenfromberg@gmail.com>
2022-12-06 00:33:04 -0500

    updating

--- day06/part1/main.py
+++ day06/part1/main.py
@@ -1,7 +1,4 @@
+n = 4
 with open('./day06/data.txt') as f:
     for line in f:
-        for i in range(0,len(line)-4+1):
-            test = line[i:i+4]
-            if(len(set([x for x in test])) == 4):
-                print(i+4)
-                break
+        print([i for i in range(n,len(line)+1) if(len(set([x for x in line[i-n:i]])) == n)][0])
--- day06/part2/main.py
+++ day06/part2/main.py
@@ -1,7 +1,4 @@
+n = 14
 with open('./day06/data.txt') as f:
     for line in f:
-        for i in range(0,len(line)-14+1):
-            test = line[i:i+14]
-            if(len(set([x for x in test])) == 14):
-                print(i+14)
-                break
+        print([i for i in range(n,len(line)+1) if(len(set([x for x in line[i-n:i]])) == n)][0])
