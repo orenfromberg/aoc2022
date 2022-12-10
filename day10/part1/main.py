5b466c604b3c2db36707934f0445e94adf9d2a65
Oren Fromberg <oren@adhocteam.us>
2022-12-10 09:33:07 -0500

    day10

---
+++ day10/part1/main.py
@@ -0,0 +1,44 @@
+import re
+
+def execute(i):
+    global x
+    if(re.match('addx',i)):
+        _, p = i.split(' ')
+        x += int(p)
+    elif(re.match('noop',i)):
+        x += 0
+
+def cycle(instructions):
+    global values
+    global cycle_count
+    cycle_count += 1
+
+    # decrement counter for all instructions
+    for i in instructions:
+        i[1] -= 1
+        if(i[1] == 0):
+            execute(i[0])
+    # clean out
+    for i in range(len(instructions)):
+        if(instructions[i][1] == 0):
+            instructions.pop(i)
+            break
+
+    values.append(x)
+
+x = 1
+instructions = []
+values = [1]
+cycle_count = 0
+with open('./day10/data.txt') as f:
+    for line in f:
+        i = line.strip()
+        if(re.match('addx',i)):
+            instructions.append([i,2])
+            for i in range(2):
+                cycle(instructions)
+        elif(re.match('noop',i)):
+            instructions.append([i,1])
+            cycle(instructions)
+
+print(sum([values[i]*(i+1) for i in range(20-1,220,40)]))
