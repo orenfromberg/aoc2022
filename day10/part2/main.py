aa6a12a6cef88d4d20a10cecc80b77084e865ad5
Oren Fromberg <oren@adhocteam.us>
2022-12-10 11:31:20 -0500

    cd

--- day10/part2/main.py
+++ day10/part2/main.py
@@ -8,43 +8,32 @@ def execute(i):
     elif(re.match('noop',i)):
         x += 0
 
-def cycle(instructions):
+def cycle(instruction):
     global values
     global cycle_count
 
     col = cycle_count % 40
-    if (x == col or x-1==col or x+1 ==col):
-        print('#',end='\n' if col == 39 else '')
-    else:
-        print('.',end='\n' if col == 39 else '')
+    print('#' if(x == col or x-1==col or x+1 ==col) else '.',end='\n' if col == 39 else '')
 
     cycle_count += 1
-
-    # decrement counter for all instructions
-    for i in instructions:
-        i[1] -= 1
-        if(i[1] == 0):
-            execute(i[0])
-    # clean out
-    for i in range(len(instructions)):
-        if(instructions[i][1] == 0):
-            instructions.pop(i)
-            break
+    instruction[1] -= 1
+    if(instruction[1] == 0):
+        execute(instruction[0])
 
     values.append(x)
 
 
 x = 1
-instructions = []
+instruction = []
 values = [1]
 cycle_count = 0
 with open('./day10/data.txt') as f:
     for line in f:
         i = line.strip()
         if(re.match('addx',i)):
-            instructions.append([i,2])
+            instruction = [i, 2]
             for i in range(2):
-                cycle(instructions)
+                cycle(instruction)
         elif(re.match('noop',i)):
-            instructions.append([i,1])
-            cycle(instructions)
+            instruction = [i, 1]
+            cycle(instruction)
