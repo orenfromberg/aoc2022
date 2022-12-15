991433638bc2f06f75d7deb2c626e700e1089cf0
Oren Fromberg <orenfromberg@gmail.com>
2022-12-15 10:30:42 -0500

    day15

--- day15/part1/main.py
+++ day15/part1/main.py
@@ -15,10 +15,6 @@ with open('./day15/data.txt') as f:
         b = (int(result.group(3)),int(result.group(4)))
         data.append([a,b,distance(a,b)])
 
-min_x = min([row[0][0] for row in data])
-max_x = max([row[0][0] for row in data])
-max_d = max([row[2] for row in data])
-
 # tests if (x,y) is not a beacon within range of this sensor
 def cant_be_beacon(x,y,data) -> bool:
     sensor = data[0]
@@ -30,17 +26,34 @@ def cant_be_beacon(x,y,data) -> bool:
     else:
         return False
 
-y = 2000000
-# y=10
-count = 0
-_from = min_x-max_d #-1605551
-_to = max_x+max_d+1 #5599294
-for x in range(min_x-max_d, max_x+max_d+1,1):
-    result = False
-    for i in [cant_be_beacon(x,y,row) for row in data]:
-        result = result or i
-    if result:
-        count+=1
-        continue
+def contains_line(y, row):
+    sensor = row[0]
+    radius = row[2]
+    max_y = sensor[1] + radius
+    min_y = sensor[1] - radius
+    return y > min_y and y < max_y
 
-print(count)
+def check_line(y, data):
+    # filter data
+    _data = [row for row in data if contains_line(y, row)]
+    count = 0
+    min_x = min([row[0][0] for row in _data])
+    max_x = max([row[0][0] for row in _data])
+    max_d = max([row[2] for row in _data])
+
+
+    _from = min_x-max_d #-1605551
+    _to = max_x+max_d+1 #5599294
+    for x in range(min_x-max_d, max_x+max_d+1,1):
+        result = False
+        for i in [cant_be_beacon(x,y,row) for row in _data]:
+            result = result or i
+        if result:
+            count+=1
+            continue
+    return count
+    
+# y = 2000000
+# y=10
+# print(check_line(10,data))
+print(check_line(2000000,data))
